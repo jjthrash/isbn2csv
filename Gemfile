17c5b66846ae594bca277218de323cdeff8be1e6
Jimmy Thrasher <jimmy@jimmythrasher.com>
2011-08-12 21:12:15 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+gem 'pg'
+
+group :development do
+  gem 'heroku'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isbn2csv::Application.config.secret_token = 'df222e6ef10788bb4b2e5e8507edd0c337b1302c3b6260c17ed20ebabe54f6eda088ee8f3b2eb33a7481b924f032edc11387c5666260a6e5e71e732a0c3fb5a0'
