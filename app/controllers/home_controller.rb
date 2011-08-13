32bcd436787144f29f7e650a22d6006dd3bc6e41
Jimmy Thrasher <jimmy@jimmythrasher.com>
2011-08-12 21:45:38 -0400

    Get ISBN lookup and CSV generation working in a basic form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,30 @@
-class HomeController < ApplicationController
-  def index
-    #isbns = params[:csv][:isbns].split(/\D+/)
-  end
+require 'csv'
 
+class HomeController < ApplicationController
   def csv
-    redirect_to :index
+    isbns = params[:csv][:isbns].split(/\D+/)
+    csv = CSV.generate do |csv|
+      csv << %w(isbn author title)
+      isbns.each do |isbn|
+        ISBNDB_QUERY.find_book_by_isbn(isbn).first.tap do |result|
+          csv << [result.isbn, result.authors_text, result.title]
+        end
+      end
+    end
+
+    filename = 'yer_books.csv'
+
+    if request.env['HTTP_USER_AGENT'] =~ /msie/i
+      headers['Pragma'] = 'public'
+      headers["Content-type"] = "text/plain" 
+      headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
+      headers['Content-Disposition'] = "attachment; filename=\"#{filename}\"" 
+      headers['Expires'] = "0" 
+    else
+      headers["Content-Type"] ||= 'text/csv'
+      headers["Content-Disposition"] = "attachment; filename=\"#{filename}\"" 
+    end
+
+    render :layout => false, :text => csv
   end
 end
--- config/initializers/isbndb.rb
+++ config/initializers/isbndb.rb
@@ -1,1 +1,3 @@
-QuickConfig.api(:isbndb, 'ISBNDB_API_KEY')
+QuickConfig.api(:isbndb, 'ISBNDB_API_KEY') do
+  ISBNDB_QUERY = ISBNdb::Query.new([ENV['ISBNDB_API_KEY']])
+end
